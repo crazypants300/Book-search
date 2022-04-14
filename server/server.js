beb42f6d7d7b0fefd17d41ca1c1dfd5a3a2b6fdd
Nicholas Easter <nicholas.easter17@gmail.com>
2022-04-14 13:30:03 -0700

    added initial typedefs and got the graphql playground to load

--- server/server.js
+++ server/server.js
@@ -2,12 +2,28 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const startServer = async () => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers
+  });
+  
+  await server.start();
+  server.applyMiddleware({ app });
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+}
+
+startServer();
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {

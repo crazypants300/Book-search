c6df8ae1c51e06d1d82bb3bb2de386c3d7ab28ab
Nicholas Easter <nicholas.easter17@gmail.com>
2022-04-16 10:18:20 -0700

    added graphql queries and mutations

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,75 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+    mutation login($email: String, password: String) {
+        token
+        user {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                bookId
+                authors
+                description
+                title
+                image
+                link
+            }
+        }
+    }
+`;
+
+export const ADD_USER = gql`
+    mutation addUser($username: String, $email: String, $password: String) {
+        token
+        user {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                bookId
+                authors
+                description
+                title
+                image
+                link
+            }
+        }
+    }
+`;
+
+export const SAVE_BOOK = gql`
+    mutation saveBook($description: String, $title: String, $bookId: ID, $image: String, $link: String) {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+            bookId
+            authors
+            description
+            title
+            image
+            link
+        }
+    }
+`;
+
+export const REMOVE_BOOK = gql`
+    mutation removeBook($bookId: ID) {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+            bookId
+            authors
+            description
+            title
+            image
+            link
+        } 
+    }
+`;

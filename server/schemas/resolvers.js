6b86f3ffa704d6acba5592eb058f5021ac8f6054
Nicholas Easter <nicholas.easter17@gmail.com>
2022-04-16 18:38:46 -0700

    still trying to get the savebook and the other resolvers to work

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,25 @@
+const { User, Book } = require('../models');
+
 const resolvers = {
-    
+    Query: {
+        me: async (parent, args, context) => {
+            console.log(`args: ${JSON.stringify(args)}`);
+            console.log(`context: ${JSON.stringify(context)}`);
+
+            const user = User.findOne(args);
+            return user;
+        }
+    },
+
+    Mutation: {
+        saveBook: async (parent, args, context) => {
+            console.log(`args: ${JSON.stringify(args)}`);
+            console.log(`context: ${JSON.stringify(context)}`);
+            console.log(`parent: ${JSON.stringify(parent)}`);
+            User.findOneAndUpdate(args)
+
+        }
+    }
 }
 
 module.exports = resolvers;
